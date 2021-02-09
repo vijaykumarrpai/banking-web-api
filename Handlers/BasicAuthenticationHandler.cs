c15f956d6b495036035091441a24b5ce09265785
Vijaykumar R Pai <vijaykumarrpai@gmail.com>
2021-02-09 17:55:35 +0530

    Change in customer login

---
+++ Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,59 @@
+﻿using BankAPI.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace BankAPI.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly BankContext _context;
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            BankContext context) :base(options,logger,encoder,clock)
+        {
+            _context = context;
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Authorization Header was not found");
+
+            try
+            {
+                var authenticationHeaderValue = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var bytes = Convert.FromBase64String(authenticationHeaderValue.Parameter);
+                string[] credentials = Encoding.UTF8.GetString(bytes).Split(":");
+                string emailAddress = credentials[0];
+                string password = credentials[1];
+                Customer cust = _context.Customers.Where(cust => cust.Email == emailAddress && cust.Password == password).FirstOrDefault();
+            
+                if(cust == null)
+                    AuthenticateResult.Fail("Invalid Email or Password");
+                else
+                {
+                    var claims = new[] { new Claim(ClaimTypes.Name, cust.Email) };
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                }
+            }
+            catch (Exception e)
+            {
+                return AuthenticateResult.Fail("Invalid Email or Password");
+            }
+            return AuthenticateResult.Fail("Need to Implement");
+        }
+    }
+}
